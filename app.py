79ca282e824f7545e5bd3bb7afbd04c4c5dc6efe
Connick Shields <connickshields@gmail.com>
2020-12-21 15:09:19 -0800

    update for easier setup

--- app.py
+++ app.py
@@ -9,17 +9,23 @@ import atexit
 
 app = Flask(__name__)
 totp = pyotp.TOTP(pyotp.random_base32())
+setup_mode = True
 
 @app.route('/sms', methods=['POST'])
 def sms():
     number = request.form['From']
-    code  = request.form['Body']
+    msg  = request.form['Body']
     resp = MessagingResponse()
-    if(totp.verify(code)):
+    if(msg === "setup" and setup_mode):
+        resp.message(totp.provisioning_uri(""))
+    else if(msg === "STOP SETUP"):
+        setup_mode = False
+        resp.message('Setup is now stopped.')
+    else if(totp.verify(code)):
         open_door()
-        resp.message('Garage door is opening, please stand by...')
+        resp.message('Garage activated, please stand by...')
     else:
-        resp.message('Invalid code...')
+        resp.message('Invalid code or action...')
     return str(resp)
 
 def open_door():
@@ -35,5 +41,4 @@ if(__name__ == '__main__'):
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(10, GPIO.OUT)
     GPIO.output(10, GPIO.LOW)
-    print('provision with: ' + str(totp.provisioning_uri("")))
     app.run()
